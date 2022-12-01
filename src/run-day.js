fc81762322d5e095ac76a65a571dd1101cafa1e4
StianM <stian.myklebostad@schibsted.com>
2022-12-01 16:18:22 +0100

    fix: Require for runDay

--- src/run-day.js
+++ src/run-day.js
@@ -6,9 +6,9 @@
  */
 const runDay = async (year, day, input) => {
   console.log('[RUN] Running day', day, 'for year', year, 'part 1')
-  await require(`./${year}/${day}/pt1`).main(input)
+  await require(`../${year}/${day}/pt1`).main(input)
   console.log('\n[RUN] Running day', day, 'for year', year, 'part 2')
-  await require(`./${year}/${day}/pt2`).main(input)
+  await require(`../${year}/${day}/pt2`).main(input)
 }
 
 module.exports = runDay
