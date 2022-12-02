e5200f0f6d1cdc886e067b9c7bd8d057fac6d902
StianM <stian.myklebostad@schibsted.com>
2022-12-02 13:29:27 +0100

    feat: Complete day 2

--- 2022/2/pt1.js
+++ 2022/2/pt1.js
@@ -1,8 +1,54 @@
+const flow = require('../../utils/flow')
+const not = require('../../utils/not')
+const lines = require('../../utils/lines')
+const { isEmpty } = require('../../utils/is')
+const sum = require('../../utils/sum')
+
+const moves = {
+  A: 1, // Rock
+  B: 2, // Paper
+  C: 3, // Scissors
+  X: 1, // Rock
+  Y: 2, // Paper
+  Z: 3, // Scissors
+}
+
+// AX DRAW
+// AY WIN
+// AZ LOSE
+// BX LOSE
+// BY DRAW
+// BZ WIN
+// CX WIN
+// CY LOSE
+// CZ DRAW
+
+const roundMultiplier = (a, b) => {
+  if (moves[a] === moves[b]) return 0.5
+  switch (a + b) {
+    case 'AY':
+    case 'BZ':
+    case 'CX':
+      return 1
+  }
+  return 0
+}
+
+const roundPoints = (a, b) => moves[b] + roundMultiplier(a, b) * 6
+
+
 /**
  * @param {string} value
  */
 const main = async (input) => {
   // Your code here
+  const rounds = lines(input).filter(flow(isEmpty, not))
+  const results = rounds.map((line) => {
+    const [a, b] = line.split(' ')
+    return roundPoints(a, b)
+  })
+  // console.log('NaN Results', results.filter(isNaN))
+  console.log('Results:', results, '| Sum', sum(results))
 }
 
 module.exports.main = main
--- 2022/2/pt2.js
+++ 2022/2/pt2.js
@@ -1,8 +1,47 @@
+const lines = require('../../utils/lines')
+const { isEmpty } = require('../../utils/is')
+const sum = require('../../utils/sum')
+const flow = require('../../utils/flow')
+const not = require('../../utils/not')
+
+const move = {
+  A: { // Rock
+    X: 3, // Scissors LOSE
+    Y: 1, // Rock DRAW
+    Z: 2, // Paper WIN
+  },
+  B: { // Paper
+    X: 1, // Rock LOSE
+    Y: 2, // Paper DRAW
+    Z: 3, // Scissors WIN
+  },
+  C: { // Scissors
+    X: 2, // Paper LOSE
+    Y: 3, // Scissors DRAW
+    Z: 1, // Rock WIN
+  },
+}
+
+const gameState = {
+  X: 0,
+  Y: 0.5,
+  Z: 1,
+}
+
+const roundPoints = (a, b) => move[a][b] + gameState[b] * 6
+
 /**
  * @param {string} value
  */
 const main = async (input) => {
   // Your code here
+  const rounds = lines(input).filter(flow(isEmpty, not))
+  const results = rounds.map((line) => {
+    const [a, b] = line.split(' ')
+    return roundPoints(a, b)
+  })
+  // console.log('NaN Results', results.filter(isNaN))
+  console.log('Results:', results, '| Sum', sum(results))
 }
 
 module.exports.main = main
